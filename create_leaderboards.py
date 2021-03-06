0b0b89fb590b1e113ae43d848159d543cd256aaa
EricZLou <ericzlou@gmail.com>
2021-03-06 17:12:09 -0500

    html docs

--- create_leaderboards.py
+++ create_leaderboards.py
@@ -37,47 +37,82 @@ def get_html_table_rows(data):
         """
     return html
 
-def get_overall_html_str(file, data):
+# For overall.json
+def get_overall_html_str(file, data, navbar):
     return f"""
         <html>
-        <head><link rel="stylesheet" href="style.css" type="text/css"></head>
+        <head><link rel="stylesheet" href="../style.css" type="text/css"></head>
         <body>
-            <h1>Overall Optimizer Elo Leaderboard for {file}</h1>
-            <table class="default-table">
-                {get_html_table_rows(data)}
-            </table>
+            <div class="left">{navbar}</div>
+            <div class="right">
+                <h1>Overall Optimizer Elo Leaderboard for {file}</h1>
+                <table class="default-table">
+                    {get_html_table_rows(data)}
+                </table>
+            </div>
         </body></html>
     """
 
-def get_html_str(file, data):
+# For all the other jsons
+def get_html_str(file, data, navbar):
     parse_file = file.replace(".json", "")
     args = parse_file.split("_")
 
     return f"""
         <html>
+        <head><link rel="stylesheet" href="../style.css" type="text/css"></head>
+        <body>
+            <div class="left">{navbar}</div>
+            <div class="right">
+                <h1>Optimizer Elo Leaderboard for {file}</h1>
+                <h3>Set of optimizers: {args[0]}</h3>
+                <h3>Number of dimensions: {args[1][1:]}</h3>
+                <h3>Max number of evaluations: {args[2][1:]}</h3>
+                <table class="default-table">
+                    {get_html_table_rows(data)}
+                </table>
+            </div>
+        </body></html>
+    """
+
+def get_html_navbar(json_names):
+    div = "<h3>Leaderboards</h3>"
+    for name in json_names:
+        name_html = name.replace(".json", ".html")
+        div += f"<a href='{name_html}'>{name}</a>"
+    return div
+
+# For index.html
+def get_index_html_str(json_names):
+    navbar = "<h3>Leaderboards</h3>"
+    for name in json_names:
+        name_html = name.replace(".json", ".html")
+        navbar += f"<a href='html_leaderboards/{name_html}'>{name}</a>"
+    return f"""
+        <html>
         <head><link rel="stylesheet" href="style.css" type="text/css"></head>
         <body>
-            <h1>Optimizer Elo Leaderboard for {file}</h1>
-            <h3>Set of optimizers: {args[0]}</h3>
-            <h3>Number of dimensions: {args[1][1:]}</h3>
-            <h3>Max number of evaluations: {args[2][1:]}</h3>
-            <table class="default-table">
-                {get_html_table_rows(data)}
-            </table>
+            <div class="left">
+                {navbar}
+            </div>
         </body></html>
     """
 
 if __name__ == '__main__':
     jsons = load_all_games()
 
-    HTML_DIR = os.path.join(THIS_PATH, "html_leaderboards")
+    HTML_DIR = os.path.join(THIS_PATH, "docs", "html_leaderboards")
     if not os.path.exists(HTML_DIR):
         os.mkdir(HTML_DIR)
 
+    with open(os.path.join(THIS_PATH, "docs", "index.html"), "w") as f:
+        f.write(get_index_html_str(jsons.keys()))
+
+    navbar = get_html_navbar(jsons.keys())
     for file, data in jsons.items():
         file_html = file.replace(".json", ".html")
         with open(os.path.join(HTML_DIR, file_html), "w") as f:
             if file == "overall.json":
-                f.write(get_overall_html_str(file, data))
+                f.write(get_overall_html_str(file, data, navbar))
             else:
-                f.write(get_html_str(file, data))
+                f.write(get_html_str(file, data, navbar))

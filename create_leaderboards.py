c6a997a0385f5acd21790e266a41a2d0accb9ef4
EricZLou <ericzlou@gmail.com>
2021-03-06 17:25:09 -0500

    cleanup

--- create_leaderboards.py
+++ create_leaderboards.py
@@ -28,52 +28,46 @@ def get_html_table_rows(data):
 
     data_dict = dict(sorted(data_dict.items(), key=lambda item: item[1], reverse=True))
 
-    html = """
-        <tr><th>Name</th><th>Rating</th></tr>
-    """
+    html = "<tr><th>Name</th><th>Rating</th></tr>"
     for name, rating in data_dict.items():
-        html += f"""
-            <tr><td>{name}</td><td>{round(rating, 3)}</td></tr>
-        """
+        html += f"<tr><td>{name}</td><td>{round(rating, 3)}</td></tr>"
     return html
 
 # For overall.json
 def get_overall_html_str(file, data, navbar):
-    return f"""
-        <html>
-        <head><link rel="stylesheet" href="../style.css" type="text/css"></head>
-        <body>
-            <div class="left">{navbar}</div>
-            <div class="right">
-                <h1>Overall Optimizer Elo Leaderboard for {file}</h1>
-                <table class="default-table">
-                    {get_html_table_rows(data)}
-                </table>
-            </div>
-        </body></html>
-    """
+    return f"""<html>
+<head><link rel="stylesheet" href="../style.css" type="text/css"></head>
+<body>
+    <div class="left">{navbar}</div>
+    <div class="right">
+        <h1>Overall Optimizer Elo Leaderboard for {file}</h1>
+        <table class="default-table">
+            {get_html_table_rows(data)}
+        </table>
+    </div>
+</body>
+</html>"""
 
 # For all the other jsons
 def get_html_str(file, data, navbar):
     parse_file = file.replace(".json", "")
     args = parse_file.split("_")
 
-    return f"""
-        <html>
-        <head><link rel="stylesheet" href="../style.css" type="text/css"></head>
-        <body>
-            <div class="left">{navbar}</div>
-            <div class="right">
-                <h1>Optimizer Elo Leaderboard for {file}</h1>
-                <h3>Set of optimizers: {args[0]}</h3>
-                <h3>Number of dimensions: {args[1][1:]}</h3>
-                <h3>Max number of evaluations: {args[2][1:]}</h3>
-                <table class="default-table">
-                    {get_html_table_rows(data)}
-                </table>
-            </div>
-        </body></html>
-    """
+    return f"""<html>
+<head><link rel="stylesheet" href="../style.css" type="text/css"></head>
+<body>
+    <div class="left">{navbar}</div>
+    <div class="right">
+        <h1>Optimizer Elo Leaderboard for {file}</h1>
+        <h3>Set of optimizers: {args[0]}</h3>
+        <h3>Number of dimensions: {args[1][1:]}</h3>
+        <h3>Max number of evaluations: {args[2][1:]}</h3>
+        <table class="default-table">
+            {get_html_table_rows(data)}
+        </table>
+    </div>
+</body>
+</html>"""
 
 def get_html_navbar(json_names):
     div = "<h3>Leaderboards</h3>"
@@ -88,15 +82,14 @@ def get_index_html_str(json_names):
     for name in json_names:
         name_html = name.replace(".json", ".html")
         navbar += f"<a href='html_leaderboards/{name_html}'>{name}</a>"
-    return f"""
-        <html>
-        <head><link rel="stylesheet" href="style.css" type="text/css"></head>
-        <body>
-            <div class="left">
-                {navbar}
-            </div>
-        </body></html>
-    """
+    return f"""<html>
+<head><link rel="stylesheet" href="style.css" type="text/css"></head>
+<body>
+    <div class="left">
+        {navbar}
+    </div>
+</body>
+</html>"""
 
 if __name__ == '__main__':
     jsons = load_all_games()
